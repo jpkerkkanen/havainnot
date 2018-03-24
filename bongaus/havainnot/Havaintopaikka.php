646b942dde39d287f2b9ddb3f7644a33e85aa430
Jukka-Pekka Kerkkänen <jpkerkkanen@gmail.com>
2018-03-24 17:45:53 +0100

    Commit 23.3.2018: Omien paikkojen toteutus näin välihommana.

---
+++ bongaus/havainnot/Havaintopaikka.php
@@ -0,0 +1,134 @@
+<?php
+
+/*
+ *create table paikat
+(
+  id                    int auto_increment not null,
+  henkilo_id            int default -1 not null,
+  nimi                  varchar(30) not null,
+  selitys               varchar(200) not null,
+  maa                   int not null,
+  primary key (id),
+  index(henkilo_id),
+  index(maa),
+  FOREIGN KEY (henkilo_id) REFERENCES henkilot (id)
+                      ON DELETE CASCADE
+) ENGINE=INNODB;
+ */
+
+/**
+ * Tämä huolehtii omien paikkojen tallentamisesta. Näin havainto voidaan
+ * yhdistää paikan id:hen, mikä helpottaa paikkojen hallinnointia ja 
+ * mahdollistaa esimerkiksi havaintojen haun paikan mukaan. Esimerkiksi
+ * lisäluokitusten ongelma on se, että vaikkapa kotipiha voi muuttua, eikä
+ * tieto siitä jää talteen. Omien paikkojen avulla päästään eroon tästä
+ * ongelmasta.
+ *
+ * @author J-P
+ */
+class Havaintopaikka {
+    public static $SARAKENIMI_HENKILO_ID= "henkilo_id";
+    public static $SARAKENIMI_NIMI= "nimi";
+    public static $SARAKENIMI_SELITYS= "selitys";
+    public static $SARAKENIMI_MAA= "maa";
+    
+    public static $taulunimi = "havaintopaikat";
+    
+    
+    /**
+     * @param Tietokantaolio $tietokantaolio
+     * @param int $id olion id tietokannassa
+     */
+    function __construct($id, $tietokantaolio){
+        $tietokantasolut = 
+            array(new Tietokantasolu(Havaintopaikka::$SARAKENIMI_ID, 
+                                                    Tietokantasolu::$luku_int),  
+                new Tietokantasolu(Havaintopaikka::$SARAKENIMI_HENKILO_ID, 
+                                                    Tietokantasolu::$luku_int),  
+                new Tietokantasolu(Havaintopaikka::$SARAKENIMI_NIMI, 
+                                                    Tietokantasolu::$mj_tyhja_EI_ok), 
+                new Tietokantasolu(Havaintopaikka::$SARAKENIMI_SELITYS, 
+                                                    Tietokantasolu::$mj_tyhja_ok),
+                new Tietokantasolu(Havaintopaikka::$SARAKENIMI_MAA, 
+                                                    Tietokantasolu::$luku_int));
+        
+        $taulunimi = Havaintopaikka::$taulunimi;
+        parent::__construct($tietokantaolio, $id, $taulunimi, $tietokantasolut);
+        
+        $this->poistetut_pikakommentit_lkm = 0;
+    }
+    
+    /**
+     * Hakee tietokannasta kyseisen käyttäjän havaintopaikat ja palauttaa ne
+     * Havaintopaikka-luokan olioina taulukossa (array).
+     * 
+     * @param Tietokantaolio $tietokantaolio
+     * @param int $id_henk Käyttäjän id
+     */
+    public static function hae_omat_paikat($tietokantaolio, $id_henk){
+        
+        $id_henk = $in_henk+0;  // Muutos integeriksi varmuuden vuoksi.
+        
+        if(is_int($id_henk) && $id_henk > 0){
+            
+            $hakulause = "SELECT ".Havaintopaikka::$SARAKENIMI_ID.
+                    " FROM ". Havaintopaikka::$taulunimi.
+                    " WHERE ".Havaintopaikka::$SARAKENIMI_HENKILO_ID = $id_henk.
+                    " ORDER BY ". Havaintopaikka::$SARAKENIMI_NIMI;
+        
+            $tulostaulu = 
+                $tietokantaolio->tee_omahaku_oliotaulukkopalautteella($hakulause);
+
+            $oliot = array();   // Tämä sisältää Havaintojakso-luokan oliot.
+
+            if(!empty($tulostaulu)){
+                foreach ($tulostaulu as $tk_olio) {
+
+                    $id = $tk_olio->id;
+                    $olio = new Havaintopaikka($id, $tietokantaolio);
+                    if($olio->olio_loytyi_tietokannasta){
+                        array_push($oliot, $olio);
+                    }
+                }
+            }
+        }
+        
+        return $oliot;
+    }
+    
+    /**
+     * Palauttaa parametrina saatujen Havaintopaikka-luokan olioiden id-arvot
+     * taulukossa.
+     * @param array $paikat Havaintopaikka-luokan oliot taulukossa.
+     */
+    public static function hae_paikkojen_idt($paikat){
+        
+        $idtaulu = array();   // Tämä sisältää idt.
+        
+        foreach ($paikat as $paikka) {
+            if($paikka instanceof Havaintopaikka){
+                array_push($idtaulu, $paikka->get_id());
+            }
+        }
+        return $idtaulu;
+    }
+    
+    /**
+     * Palauttaa parametrina saatujen Havaintopaikka-luokan olioiden 
+     * valikkoon sopivat nimitykset taulukossa.
+     */
+    public static function hae_paikkojen_valikkonimet($paikat){
+        
+        $nimitaulu = array();   // Tämä sisältää nimet.
+        
+        foreach ($paikat as $paikka) {
+            if($paikka instanceof Havaintopaikka){
+
+                $nimi = $paikka->get_arvo(Havaintopaikka::$SARAKENIMI_NIMI);
+                
+                array_push($nimitaulu, $nimi);
+            }
+        }
+        return $nimitaulu;
+    }
+}
